fd985332223b4ceef10f8831b473e6301e848130
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2021-05-21 09:59:08 -0500

    update src/lib/obazl_opoam

--- src/lib/obazl_opam/obazl_opam.c
+++ src/lib/obazl_opam/obazl_opam.c
@@ -10,28 +10,6 @@ LOCAL UT_string *opam_switch;
 LOCAL UT_string *opam_bin;
 LOCAL UT_string *opam_lib;
 
-char *run_cmd(char *cmd)
-{
-    static char buf[BUFSIZ];
-    FILE *fp;
-
-    if ((fp = popen(cmd, "r")) == NULL) {
-        printf("Error opening pipe!\n");
-        return NULL;
-    }
-
-    while (fgets(buf, sizeof buf, fp) != NULL) {
-        /* printf("SWITCH: %s\n", buf); */
-        buf[strcspn(buf, "\n")] = 0;
-    }
-
-    if(pclose(fp))  {
-        printf("Command not found or exited with error status\n");
-        return NULL;
-    }
-    return buf;
-}
-
 /* dup from opam_bootstrap.c */
 void config_opam(char *_opam_switch)
 {
@@ -50,14 +28,15 @@ void config_opam(char *_opam_switch)
     /* FIXME: handle switch arg */
     char *cmd, *result;
     if (_opam_switch == NULL) {
-        log_info("opam: using current switch");
+        /* log_info("opam: using current switch"); */
         cmd = "opam var switch";
 
         result = run_cmd(cmd);
         if (result == NULL) {
             fprintf(stderr, "FAIL: run_cmd(%s)\n", cmd);
-        } else
+        } else {
             utstring_printf(opam_switch, "%s", result);
+        }
     }
 
     cmd = "opam var bin";
@@ -83,11 +62,11 @@ void config_opam(char *_opam_switch)
 UT_array *inventory_opam(void)
 {
     config_opam(NULL);
-    log_debug("opam switch: %s", utstring_body(opam_switch));
-    log_debug("opam bin: %s", utstring_body(opam_bin));
-    log_debug("opam lib: %s", utstring_body(opam_lib));
+    /* log_debug("opam switch: %s", utstring_body(opam_switch)); */
+    /* log_debug("opam bin: %s", utstring_body(opam_bin)); */
+    /* log_debug("opam lib: %s", utstring_body(opam_lib)); */
 
-
+    // FIXME: make re-entrant
     UT_array *opam_dirs;             /* string list */
     utarray_new(opam_dirs, &ut_str_icd);
 
