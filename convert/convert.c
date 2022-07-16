2ebe197ca3a33793aa0b5e51323d12a4a1d04ff4
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2022-07-16 08:32:47 -0500

    fixes for convert, repl targets

--- convert/convert.c
+++ convert/convert.c
@@ -6,6 +6,8 @@
 /* #include "ansi_colors.h" */
 /* #include "load_dune.h" */
 
+#include "ini.h"
+#include "log.h"
 #include "mibl.h"
 #include "convert.h"
 
@@ -17,6 +19,19 @@ extern char *ews_root;
 extern int dir_ct;
 extern int file_ct;
 extern int dunefile_ct;
+
+s7_pointer _load_load_dune(s7_scheme *s7)
+{
+    s7_pointer _load_dune;
+    _load_dune = s7_name_to_value(s7, "load-dune");
+    if (_load_dune == s7_undefined(s7)) {
+        log_error("unbound symbol: load-dune");
+        log_info("*load-path*: %s", TO_STR(s7_load_path(s7)));
+        s7_error(s7, s7_make_symbol(s7, "unbound-symbol"),
+                 s7_list(s7, 1, s7_make_string(s7, "load-dune")));
+    }
+    return _load_dune;
+}
 
 int main(int argc, char *argv[])
 {
@@ -51,120 +66,64 @@ int main(int argc, char *argv[])
     }
     /* config in this order: bazel then s7 */
     bazel_configure(); // getcwd(NULL, 0));
-    s7_configure();
+    s7_scheme *s7 = s7_configure();
 
-    /* test */
-    /* char *canonical_path(char *path); */
-    /* printf("np: %s\n", canonical_path("dune/stanzas/rule/deps/glob/../foo.ml")); */
-    /* printf("np: a/b/../c       %s\n", canonical_path("a/b/../c")); */
-    /* printf("np: a/b/../../c:   %s\n", canonical_path("a/b/../../c")); */
-    /* printf("np: a/b/c/../../d: %s\n", canonical_path("a/b/c/../../d")); */
-
-    /* printf("np: a/b/c:         %s\n", canonical_path("a/b/c")); */
-    /* printf("np: .a/b/c:        %s\n", canonical_path("./a/b/c")); */
-    /* printf("np: ./././a/b/c:   %s\n", canonical_path("./././a/b/c")); */
-    /* printf("np: .///a/b/c:     %s\n", canonical_path(".///a/b/c")); */
-    /* printf("np: ././/a/./b/././//c: %s\n", canonical_path("././/a/./b/././//c")); */
-    /* printf("np: %s\n", canonical_path(".//a/b/c")); /\* busted *\/ */
-    /* printf("np: a/b.c/e.f:     %s\n", canonical_path("a/b.c/e.f")); */
-    /* printf("np: a/.b/e.f:      %s\n", canonical_path("a/.b/e.f")); */
-    /* printf("np: ./a/b/c:       %s\n", canonical_path("./a/b/c")); */
-    /* printf("np: ../a/b/c:      %s\n", canonical_path("../a/b/c")); */
-    /* printf("np: a////b/./c//d: %s\n", */
-    /*        canonical_path("a////b/./c//d")); */
-    /* printf("np: a/./c:         %s\n", canonical_path("a/./c")); */
-    /* printf("np: a//c:          %s\n", canonical_path("a//c")); */
-    /* printf("np: a/////b:       %s\n", canonical_path("a/////b")); */
-    /* printf("np: a/././././b:   %s\n", canonical_path("a/././././b")); */
-
-    //TODO: cli args for root and path
     char *rootdir;
     char *pathdir;
 
-    /* rootdir = "obazl/mibl/test"; */
-    /* /\* pathdir = "test/files"; *\/ */
-    /* pathdir = "test/dune/stanzas/rule/deps/glob"; */
+    rootdir = getcwd(NULL, 0);
+    pathdir = "./";
 
-    /* pathdir = "ws/a/b"; */
+    s7_pointer _s7_load_dune = _load_load_dune(s7);
+    printf("load-dune: %s\n", TO_STR(_s7_load_dune));
 
-    /* launching from test/ws */
-    /* pathdir = "a/b"; // ws/a/WORKSPACE.bazel */
+    s7_pointer wss = s7_eval_c_string(s7, "(load-dune)");
+    printf("wss: %s\n", TO_STR(wss));
 
-    /* pathdir = "test/baddot"; */
-    /* pathdir = "test/includes/mwe"; */
-    /* pathdir = "test/filetypes"; */
-    /* pathdir = "test/mwe"; */
+    /*
+      1. get :@ ws
+      2. get :pkgs from :@
+      2. for-each pkg in :pkgs ...
+     */
 
-    /* pathdir = "test/tezos/lib_clic"; */
-    /* pathdir = "test/tezos/lib_requester"; */
-    /* pathdir = "test/tezos/lib_stdlib_unix"; */
-    /* pathdir = "test/tezos/lib_test"; */
-    /* pathdir = "test/tezos/lib_workers"; */
-    /* pathdir = "test/tezos/openapi"; */
-    /* pathdir = "test/tezos/proto_000_Ps9mPmXa"; */
-
-    /* pathdir = "test/mina"; */
-    /* pathdir = "test/mina/ocaml-sodium"; */
-
-    /* **************** */
-    /* rootdir = "tweag/tezos"; */
-    /* pathdir = "src/bin_node"; */
-    /* pathdir = "src/lib_clic"; */
-    /* pathdir = "src/lib_stdlib_unix"; */
-    /* pathdir = "src/proto_alpha"; */
-    /* pathdir = "src/lib_protocol_environment"; */
-
-    /* **************** */
-    /* rootdir = "minadev/gitfork"; */
-    /* pathdir = "src"; */
-    /* pathdir = "src/lib/snarky/src"; */
-
-    rootdir = "obazl/mibl/test";
-    /* pathdir = "test/dune/stanzas/rule/deps/glob"; */
-    /* pathdir = "test/dune/stanzas/rule/action/run/cp"; */
-    pathdir = "test/dune/stanzas/library/deps/select";
-
-    s7_pointer pkg_tbl = load_dune(rootdir, pathdir);
-    printf("cwd: %s\n", getcwd(NULL, 0));
-
-    printf(BGRN "pkg_tbl:" CRESET "\n%s\n", s7_object_to_c_string(s7, pkg_tbl));
+    /* printf(BGRN "pkg_tbl:" CRESET "\n%s\n", s7_object_to_c_string(s7, pkg_tbl)); */
 
     /* (pkg (hash-table-ref pkgs arg)) */
-    s7_pointer ht_ref = s7_name_to_value(s7, "hash-table-ref");
-    if (ht_ref == s7_undefined(s7)) {
-        printf("unbound symbol: hash-table-ref");
-        exit(EXIT_FAILURE);
-    }
-    s7_pointer pkg_key = s7_make_string(s7, "dune/stanzas/library/deps/select");
-    s7_pointer pkg = s7_call(s7, ht_ref, s7_list(s7, 2, pkg_tbl, pkg_key));
-    printf(BGRN "pkg:" CRESET " %s\n", TO_STR(pkg));
+    /* s7_pointer ht_ref = s7_name_to_value(s7, "hash-table-ref"); */
+    /* if (ht_ref == s7_undefined(s7)) { */
+    /*     printf("unbound symbol: hash-table-ref"); */
+    /*     exit(EXIT_FAILURE); */
+    /* } */
+    /* s7_pointer pkg_key = s7_make_string(s7, "dune/stanzas/library/deps/select"); */
+    /* s7_pointer pkg = s7_call(s7, ht_ref, s7_list(s7, 2, pkg_tbl, pkg_key)); */
+    /* printf(BGRN "pkg:" CRESET " %s\n", TO_STR(pkg)); */
 
-    /* (stanzas (assoc :dune-stanzas pkg)) */
-    s7_pointer assoc = s7_name_to_value(s7, "assoc");
-    if (assoc == s7_undefined(s7)) {
-        printf("unbound symbol: assoc");
-        exit(EXIT_FAILURE);
-    }
-    s7_pointer dune_stanzas_kw = s7_make_keyword(s7, "dune-stanzas");
-    s7_pointer stanzas = s7_call(s7, assoc,
-                                 s7_list(s7, 2,
-                                         dune_stanzas_kw, pkg));
-    printf(BGRN "stanzas:" CRESET " %s\n", TO_STR(stanzas));
+    /* /\* (stanzas (assoc :dune-stanzas pkg)) *\/ */
+    /* s7_pointer assoc = s7_name_to_value(s7, "assoc"); */
+    /* if (assoc == s7_undefined(s7)) { */
+    /*     printf("unbound symbol: assoc"); */
+    /*     exit(EXIT_FAILURE); */
+    /* } */
+    /* s7_pointer dune_stanzas_kw = s7_make_keyword(s7, "dune-stanzas"); */
+    /* s7_pointer stanzas = s7_call(s7, assoc, */
+    /*                              s7_list(s7, 2, */
+    /*                                      dune_stanzas_kw, pkg)); */
+    /* printf(BGRN "stanzas:" CRESET " %s\n", TO_STR(stanzas)); */
 
-    /* (nzs (dune-pkg->mibl pkg)) */
-    s7_pointer nds = s7_name_to_value(s7, "dune-pkg->mibl");
-    if (nds == s7_undefined(s7)) {
-        printf("unbound symbol: dune-pkg->mibl");
-        exit(EXIT_FAILURE);
-    }
-    s7_pointer normalized = s7_call(s7, nds,
-                                    s7_list(s7, 1, pkg));
-    printf(BGRN "normalized stanza:" CRESET " %s\n", TO_STR(normalized));
+    /* /\* (nzs (dune-pkg->mibl pkg)) *\/ */
+    /* s7_pointer nds = s7_name_to_value(s7, "dune-pkg->mibl"); */
+    /* if (nds == s7_undefined(s7)) { */
+    /*     printf("unbound symbol: dune-pkg->mibl"); */
+    /*     exit(EXIT_FAILURE); */
+    /* } */
+    /* s7_pointer normalized = s7_call(s7, nds, */
+    /*                                 s7_list(s7, 1, pkg)); */
+    /* printf(BGRN "normalized stanza:" CRESET " %s\n", TO_STR(normalized)); */
 
-    /* printf("*load-path*: %s\n", */
-    /*        s7_object_to_c_string(s7, */
-    /*                              s7_load_path(s7) */
-    /*                              )); */
+    /* /\* printf("*load-path*: %s\n", *\/ */
+    /* /\*        s7_object_to_c_string(s7, *\/ */
+    /* /\*                              s7_load_path(s7) *\/ */
+    /* /\*                              )); *\/ */
 
     if (verbose) {
         printf("ews: %s\n", ews_root);
