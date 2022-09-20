7e1f453493b6570e2796e90c92419c3b4a3bef26
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2022-09-19 21:08:42 -0500

    convert.c: support -p <pkg> option, to restrict bazel emits

--- convert/convert.c
+++ convert/convert.c
@@ -18,8 +18,9 @@ extern int dunefile_ct;
 
 int main(int argc, char *argv[])
 {
-    char *opts = "p:hdtvx";
-    int opt;
+    char *opts = "p:r:hdtvx";
+    int opt, len;
+    char *rootpath = NULL;
     char *pkgarg = NULL;
 
     bool exit_on_error = false;
@@ -35,6 +36,7 @@ int main(int argc, char *argv[])
             exit(EXIT_FAILURE);
             break;
         case 'd':
+            printf("SETTING DEBUG\n");
             debug = true;
             break;
         case 'h':
@@ -45,11 +47,20 @@ int main(int argc, char *argv[])
         case 'p':
             pkgarg = strdup(optarg);
             /* remove trailing '/' */
-            int len = strlen(pkgarg);
+            len = strlen(pkgarg);
             if (pkgarg[len-1] == '/') {
                 pkgarg[len-1] = '\0';
             }
-            log_debug("package: %s\n", pkgarg);
+            /* validate - no abs paths, may start with '//" */
+            break;
+        case 'r':
+            rootpath = strdup(optarg);
+            /* remove trailing '/' */
+            len = strlen(rootpath);
+            if (rootpath[len-1] == '/') {
+                rootpath[len-1] = '\0';
+            }
+            /* validate - relative path, not internal .. */
             break;
         case 't':
             trace = true;
@@ -62,14 +73,23 @@ int main(int argc, char *argv[])
             ;
         }
     }
-    if ((argc - optind) > 1) {
-        log_error("Too many options");
+    log_debug("argc: %d", argc);
+    log_debug("optind: %d", optind);
+
+    if (argc != optind) {
+        log_error("non-opt argument");
+        /* log_error("next: %s", argv[optind]); */
         exit(EXIT_FAILURE);
-    } else {
-        if ((argc-optind) == 1) {
-            pkgarg = argv[optind];
-            log_info("PATH: %s", pkgarg);
-        }
+    }
+
+    /* if ((argc-optind) == 1) { */
+    /*     rootpath = argv[optind]; */
+    /*     log_info("ROOTPATH: %s", rootpath); */
+    /* } */
+
+    if (debug) {
+        log_debug("rootpath: '%s'", rootpath);
+        log_debug("pkgarg: '%s'", pkgarg);
     }
 
     /* config in this order: first bazel, then mibl, then s7 */
@@ -85,6 +105,13 @@ int main(int argc, char *argv[])
     /* printf("*exit-on-error*? %d\n", */
     /*        (s7_t(s7) == s7_name_to_value(s7, "*exit-on-error*"))); */
 
+    if (pkgarg) {
+        UT_string *setter; utstring_new(setter);
+        utstring_printf(setter, "(set! *emit-bazel-pkg* \"%s\")", pkgarg);
+        /* log_debug("SETTING *emit-bazel-pkg* to: %s", pkgarg); */
+        s7_eval_c_string(s7, utstring_body(setter));
+    }
+
     s7_load(s7, "starlark.scm");
 
     s7_load(s7, "convert.scm");
@@ -96,16 +123,31 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    s7_pointer arg;
-    if (pkgarg)
-        arg = s7_list(s7, 1, s7_make_string(s7, pkgarg));
-    else
-        arg = s7_nil(s7);
+    s7_pointer _s7_pkgarg;
+    if (pkgarg) {
+        _s7_pkgarg = s7_make_string(s7, pkgarg);
+    } else {
+        _s7_pkgarg = s7_nil(s7);
+    }
+
+    s7_pointer _s7_args;
+
+    if (rootpath) {
+        _s7_args = s7_list(s7, 2,
+                           s7_make_string(s7, rootpath),
+                           _s7_pkgarg);
+    } else {
+        _s7_args = s7_list(s7, 2,
+                      s7_nil(s7),
+                      _s7_pkgarg);
+    }
+
+    if (debug) log_debug("s7 args: %s", TO_STR(_s7_args));
 
     s7_gc_on(s7, s7_f(s7));
 
     /* s7_int main_gc_loc = s7_gc_protect_via_stack(s7, _main); */
-    s7_pointer result = s7_apply_function(s7, _main, arg);
+    s7_pointer result = s7_apply_function(s7, _main, _s7_args);
     /* log_info("RESULT: %s\n", TO_STR(result)); */
     /* s7_gc_unprotect_via_stack(s7, _main); */
 
